53a748e014da4e1965a1c87897c581d5e397397b
sifuentesgemner <sifuentesgemner@gmail.com>
2024-10-02 21:52:51 -0500

    enlinia

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="resources/css/main.css">
+</head>
+
+<body class="px-3">
+
+    <div class="row mt-5">
+        <div class="col-sm-12 col-md-6">
+            <h2 class="ml-5 h2">EN LíNEA</h1>
+
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right pr-5 mt-2    ">SOLUCIONES MUNDIALES</p>
+        </div>
+    </div>
+    <div class="row">
+
+
+        <div class="col-sm-12">
+
+            <img src="resources/img/baner01.png" style="height: 500px;" class="w-100" alt="">
+        </div>
+    </div>
+
+    <div class="container mt-4">
+
+
+        <h2 class="h2">SOLUCIONES CREATIVAS</h2>
+        <p class="text-justify">sin inportar cual se a tu negocio, nuestro equipon puede hacer todo desde cero.</p>
+
+
+
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+               <div class="card border-0">
+                 <div class="card-body">
+                    <img src="resources/img/idea_01.png"  class="w-100" alt="">
+                    <h5 class=" h5 mt-4 "><i class="bi bi-pen"></i>DISEÑO</h5>
+                    <p >creamos tus diseños segun tus necesidades.</p>
+                 </div>
+               
+                    
+              
+               </div>
+
+            </div>
+            <div class="col-sm-12 col-md-4">
+               <div class="card border-0">
+                 <div class="card-body">
+                    <img src="resources/img/idea_01.png"  class="w-100" alt="">
+                    <h5 class=" h5 mt-4 "><i class="bi bi-flower3"></i>DESARROLLO</h5>
+                    <p >Nuestros progamadores te garantizaran la perfección.</p>
+                 </div>
+               
+                    
+              
+               </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-4">
+               <div class="card border-0">
+                 <div class="card-body">
+                    <img src="resources/img/idea_01.png" class="w-100" alt="">
+                    <h5 class=" h5 mt-4 "><i class="bi bi-file-plus-fill"></i>EDICIÓN</h5>
+                    <p >que el producto final llegue a tus clentes.</p>
+                 </div>
+               
+                    
+              
+               </div>
+
+            </div>
+
+        </div>
+    </div>
+
+   <footer class="bg-wine">
+    <p class=" p-5 text-white ">
+        edificio IDEX PJ, jr. Amazonas 120 - chachapoyas Amazonas 120yas Amazonas
+    </p>
+   </footer>
+</body>
+
+</html>
